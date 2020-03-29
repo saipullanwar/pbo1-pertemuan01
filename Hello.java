d17c8568a6b659b6ae72a403e5ef041eeaec8963
saipullanwar <hiccup.221200@gmail.com>
2020-03-29 14:04:29 +0800

    Hello Git & Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
- public static void main (string [] args){
-  system.out.println("Hello World !!");
+ public static void main (String [] args){
+  System.out.println("Hello Git !!");
+  System.out.println("Hello Java !!");
+  System.out.println("Hello World !!");
  }
 }
